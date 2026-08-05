9e0ac37f304a126176400da82e1ed5e5e57a7aed
vinngaritt21-sketch <vinngaritt21@gmail.com>
2026-08-05 09:06:28 +0700

    Update database live: 2026-08-05 09:06:28

--- database.js
+++ database.js
@@ -1,1 +1,1 @@
-dataIzinStaf = [{"nama": "MAHESA", "shift": "PAGI", "jenis": "Toilet", "jam_mulai": "08:57", "jam_kembali": "-", "status": "SEDANG IZIN"}];
+dataIzinStaf = [{"nama": "MAHESA", "shift": "PAGI", "jenis": "Toilet", "jam_mulai": "08:57", "jam_kembali": "09:06 WIB", "status": "TEPAT WAKTU"}];
